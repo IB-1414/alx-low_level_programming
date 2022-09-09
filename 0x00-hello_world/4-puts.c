6d8ef270554fd26c5c68cc224306fbab2a5b835c
IB-1414 <ibresguru@gmail.com>
2022-09-09 14:10:13 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-/**
+/*
  * main - prints exactly
  *
  * Return: Always 0 (successful)
  */
 int main(void)
 
-	puts("\"Programming is like building a multilingual puzzle");
+
+	puts("\"Programming is like building a multilingual
+		       	puzzle");
 
 	return (0);
 }	
