bfdddedca37f517944ec81abf0da44dd4d922bcd
IB-1414 <ibresguru@gmail.com>
2022-09-19 14:54:31 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,13 +14,11 @@ void rev_string(char *s)
 
 	while (s[length1] != '\0')
 		length1++;
-
-	length2 = length1 - 1
-	for (i = 0; i < length1 / 2; i ++)
+	length2 = length1 - 1;
+	for (i = 0; i < length1 / 2; i++)
 	{
-		"ibroalx" "xlaorbi"
 		tmp = s[i];
 		s[length2] = tmp;
 		length2 -= 1;
-	}	
+}
 }
