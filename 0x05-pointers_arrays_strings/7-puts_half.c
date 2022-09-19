97e50b9097b78fa739ccdcf5a845a846b658527a
IB-1414 <ibresguru@gmail.com>
2022-09-19 15:44:11 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,13 @@ void puts_half(char *str)
 {
 	int x, y, i;
 
-	x = strlen(str)
+	x = strlen(str);
 	if (x % 2 == 1)
 	y = x / 2 + 1;
-	else
+	if else
 	y = x / 2;
 	for (i = y; i < x; i++)
-	_putchar(str[i]);
+_putchar(str[i]);
 
-	_putchar('\n');
+_putchar('\n');
 }
